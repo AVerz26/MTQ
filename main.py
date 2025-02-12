19dac9b76dfac9e3ad64fef372ef4a78bb24335f
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2025-02-12 09:51:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,28 @@ def remover_tracos(texto):
     return texto.replace("-", "/")  # Substitui traços por barras
 
 # Campos de entrada
-campo_data = st.date_input("Data de Abertura Vaga")
-campo_data = campo_data.strftime("%d/%m/%Y")
-descricao = st.text_input("Descrição da Vaga:", "")
-descricao = remover_tracos(descricao)  # Remove traços da descrição
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    campo_data = st.date_input("Data de Abertura Vaga")
+    campo_data = campo_data.strftime("%d/%m/%Y")
+with col2:
+    descricao = st.text_input("Descrição da Vaga:", "")
+    descricao = remover_tracos(descricao)  # Remove traços da descrição
 
 # Menus suspensos (selectbox)
-campo_menu_1 = st.selectbox("Localização da Vaga:", ["ITANHANDU/MG", "PRIMAVERA DO LESTE/MT", "LORENA/SP", "FORMOSA/GO", "FRANCISCO BELTRÃO/PR", "CANELA/SC", "DESCALVADO/SP", "CAMPANHA/MG", "PASSA QUATRO/MG", "UBERLÂNDIA/MG", "CÉU AZUL/PR", "ÁGUA BOA/MT", "CONFRESA/MT", "RIZOMA", "FAZENDA DA TOCA"])
-campo_menu_2 = st.text_input("Setor:")
-campo_menu_2 = remover_tracos(campo_menu_2)  # Remove traços do setor
+with col3:
+    campo_menu_1 = st.selectbox("Localização da Vaga:", ["ITANHANDU/MG", "PRIMAVERA DO LESTE/MT", "LORENA/SP", "FORMOSA/GO", "FRANCISCO BELTRÃO/PR", "CANELA/SC", "DESCALVADO/SP", "CAMPANHA/MG", "PASSA QUATRO/MG", "UBERLÂNDIA/MG", "CÉU AZUL/PR", "ÁGUA BOA/MT", "CONFRESA/MT", "RIZOMA", "FAZENDA DA TOCA"])
 
-campo_menu_3 = st.text_input("Turno:")
-campo_menu_3 = remover_tracos(campo_menu_3)  # Remove traços do turno
+coli1, coli2 = st.columns(2)
+
+with coli1:
+    campo_menu_2 = st.text_input("Setor:")
+    campo_menu_2 = remover_tracos(campo_menu_2)  # Remove traços do setor
+
+with coli2:
+    campo_menu_3 = st.text_input("Turno:")
+    campo_menu_3 = remover_tracos(campo_menu_3)  # Remove traços do turno
 
 # Botão para gerar a concatenação
 if st.button("Gerar Concatenação"):
