d170e78a182f856dbabc6f40bf4f1393c4cf7c35
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2025-02-12 09:29:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import streamlit as st
-# Título do site
-st.title("Gerador de Concatenação de Dados")
-
 # Campos de entrada
-campo_data = st.date_input("Selecione uma data")
+campo_data = st.date_input("Data de Abertura Vaga")
 campo_data = campo_data.strftime("%d/%m/%Y")
-campo_digitavel = st.text_input("Digite algo", "")
+descricao = st.text_input("Descrição da Vaga:", "")
 
 # Menus suspensos (selectbox)
-campo_menu_1 = st.selectbox("Escolha uma opção no primeiro menu", ["Opção 1", "Opção 2", "Opção 3"])
-campo_menu_2 = st.selectbox("Escolha uma opção no segundo menu", ["A", "B", "C"])
+campo_menu_1 = st.selectbox("Localização da Vaga:", ["Opção 1", "Opção 2", "Opção 3"])
+campo_menu_2 = st.selectbox("Setor:", ["A", "B", "C"])
+
+campo_menu_3 = st.text_input("Turno:")
 
 # Botão para gerar a concatenação
 if st.button("Gerar Concatenação"):
     # Concatenação dos dados separados por traço
-    concatenacao = f"{campo_data} - {campo_digitavel} - {campo_menu_1} - {campo_menu_2}"
+    concatenacao = f"{campo_data} - {campo_digitavel} - {campo_menu_1} - {campo_menu_2} - {campo_menu_3}"
     
     # Exibir a concatenação
     st.write("Resultado da concatenação:")
