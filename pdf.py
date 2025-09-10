91276219062b52322c2a2d8c721e2f83e71fda8b
André Verzoto <118853023+AVerz26@users.noreply.github.com>
2025-09-10 14:38:02 -0400

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -116,7 +116,7 @@ class PedidoPDF(FPDF):
         # Transformar em duas colunas
         self.ln(10)
         self.set_font("Helvetica", "", 10)
-        self.multi_cell(0, 5, "Dados Bancários: \n Itaú Unibanco S.A (Cód.: 341) \n Agência: 3032 \n C. Corrente: 37004-5\n PIX: mtq341370045@grupomantiqueira.com.br (Mantiqueira Alimentos Ltda.)")
+        self.multi_cell(0, 5, "Dados Bancários: \n Itaú Unibanco S.A (Cód.: 341)   Agência: 3032   C. Corrente: 37004-5 \nPIX: mtq341370045@grupomantiqueira.com.br (Mantiqueira Alimentos Ltda.) \n")
 
     def tabela_itens(self, df):
         self.set_font("Arial", "B", 9)
